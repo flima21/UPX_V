848075200afe08f8c7427173cea1b7978863e32d
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-10-07 15:50:06 -0300

    add: front login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:lorem_ipsum/lorem_ipsum.dart';
+import 'package:upxv/pages/help_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 500,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        child: ListView(
+                          padding: EdgeInsets.all(10),
+                          children: [
+                            Text(loremIpsum(words: 2),style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                            Text(loremIpsum(words: 100),textAlign: TextAlign.justify,),
+                          ],
+                        ),
+                      ),
+                      TextButton(
+                        child: const Text('FECHAR'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          );
+        },
+        child: Icon(Icons.help_sharp),
+      ),
+      appBar: AppBar(
+        title: Text("CROSS LINE"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: FlutterLogo(size: 23,)
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'E-mail',
+                              hintStyle: TextStyle(
+                                color: Colors.grey[400]
+                              )
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          SizedBox(height: 10,),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle: TextStyle(
+                                color: Colors.grey[400]
+                              )
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 10,),
+                          Row(
+                            children: [
+                              Expanded(child: ElevatedButton(onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => HelpPage()));
+                              }, child: Text("ENTRAR"),)),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Expanded(child: ElevatedButton(onPressed: () {}, child: Text("REGISTRAR"))),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Expanded(child: TextButton(onPressed: () {}, child: Text("ESQUECI A SENHA")))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
