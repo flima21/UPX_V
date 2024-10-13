4e2d90981750ff11e7fcb132280f12705a61ab60
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-10-13 10:02:59 -0300

    principal main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:upxv/pages/hello_page.dart';
-import 'package:upxv/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,37 +13,99 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      darkTheme: ThemeData(
-      //   useMaterial3: true,
-      //   // scaffoldBackgroundColor: Color.fromARGB(162, 22, 140, 169),
-      //   floatingActionButtonTheme: FloatingActionButtonThemeData(
-      //     // backgroundColor: const Color.fromARGB(255, 0, 115, 145),
-      //     // foregroundColor: Colors.white
-      //   ),
-      //   elevatedButtonTheme: ElevatedButtonThemeData(
-      //     style: ButtonStyle(
-      //       // backgroundColor: WidgetStatePropertyAll<Color>(Color.fromARGB(255, 0, 115, 145)),
-      //     )
-      //   ),
+      theme: ThemeData(
+        brightness: Brightness.light,
         appBarTheme: AppBarTheme(
           shadowColor: Theme.of(context).colorScheme.shadow,
           scrolledUnderElevation: 10,
-          // backgroundColor: const Color.fromARGB(255, 0, 115, 145),
-          // foregroundColor: Colors.white
         ),
         cardTheme: CardTheme(
-          elevation: 10,
-          shadowColor: Colors.red,
-      //     // color: Color.fromARGB(255, 166, 223, 237)
+          elevation: 2,
+          shadowColor: Colors.amber,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            animationDuration: Duration(seconds: 5),
+            foregroundColor: WidgetStatePropertyAll<Color>(Colors.black),
+            backgroundColor: WidgetStatePropertyAll<Color>(Colors.amber)
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(color: Colors.black),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.black), 
+            // borderRadius: BorderRadius.all(Radius.circular(10))
+          )
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.amber,
+          foregroundColor: Colors.black
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: WidgetStatePropertyAll<Color>(Colors.black),
+          )
+        ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.black),
+          bodyMedium: TextStyle(color: Colors.black),
+          bodySmall: TextStyle(color: Colors.black),
+          titleLarge: TextStyle(color: Colors.black),
+          titleMedium: TextStyle(color: Colors.black),
+          titleSmall: TextStyle(color: Colors.black),
         )
+
       ),  
       
-      // theme: ThemeData(
-      //   brightness: ThemeData.dark,
-      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      //   useMaterial3: true,
-      // ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          shadowColor: Theme.of(context).colorScheme.shadow,
+          scrolledUnderElevation: 10,
+          elevation: 10
+        ),
+        cardTheme: CardTheme(
+          // elevation: 2,
+          // shadowColor: Colors.grey,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            animationDuration: Duration(seconds: 5),
+            foregroundColor: WidgetStatePropertyAll<Color>(Colors.black),
+            backgroundColor: WidgetStatePropertyAll<Color>(Colors.amber)
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(color: Colors.amber),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.amber),
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.amber), 
+            // borderRadius: BorderRadius.all(Radius.circular(10))
+          )
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.amber,
+          foregroundColor: Colors.black
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: WidgetStatePropertyAll<Color>(Colors.white),
+          )
+        ),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white),
+          titleLarge: TextStyle(color: Colors.white),
+          titleMedium: TextStyle(color: Colors.white),
+          titleSmall: TextStyle(color: Colors.white),
+        )
+      ),
       home: const HelloPage(),
     );
   }
