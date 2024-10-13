72e2a5cc52decff197fa4af2ebe842a7d3419ddd
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-10-13 10:02:07 -0300

    add: class to reponse database

---
+++ lib/utils/monitory.dart
@@ -0,0 +1,8 @@
+// ignore: file_names
+class Monitory {
+  late DateTime dateTime;
+  late String message;
+  late String location;
+
+  Monitory({required this.dateTime, required this.message, required this.location});
+}
